606af0ae8780fe43b294a053bda2d809f4be4ca6
Lily Wang <awildsheepchase@go.edu.tw>
2023-12-22 01:10:23 +0800

    Time: 687 ms (37.27%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 SELECT *
 FROM Patients
-WHERE REGEXP_LIKE(conditions, '^DIAB1') OR REGEXP_LIKE(conditions, ' DIAB1');
+# WHERE REGEXP_LIKE(conditions, '^DIAB1') OR REGEXP_LIKE(conditions, ' DIAB1');
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%';
