9d7de9ac0a30636898e29b82a4cd95362a1e3016
Lily Wang <awildsheepchase@go.edu.tw>
2023-12-26 00:15:56 +0800

    Time: 1284 ms (70.55%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -3,3 +3,4 @@ SELECT product_name, SUM(unit) AS unit
 FROM Orders O LEFT JOIN Products P ON O.product_id = P.product_id
 WHERE O.order_date BETWEEN '2020-02-01' AND '2020-02-29'
 GROUP BY product_name HAVING SUM(unit) >= 100;
+# Considering the possibility of February having 29 days is a point.
