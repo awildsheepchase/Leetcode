3dd4e6c680c501e921f96142976e3f5379dd582e
Lily Wang <awildsheepchase@go.edu.tw>
2023-12-27 23:07:26 +0800

    Time: 865 ms (17.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY id ASC) AS id,
+    student
+FROM 
+(SELECT 
+        CASE WHEN MOD(id,2) = 0 THEN id-1
+        WHEN MOD(id,2) = 1 THEN id+1
+        END AS id, student
+FROM Seat
+ORDER BY id ) AS A
