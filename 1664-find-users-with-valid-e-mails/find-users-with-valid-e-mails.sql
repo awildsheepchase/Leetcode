55b0390c9ac7cbee651eb94bcf008bf75aac9e34
Lily Wang <awildsheepchase@go.edu.tw>
2023-12-27 17:51:29 +0800

    Time: 1756 ms (10.37%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -1,10 +1,4 @@
 # Write your MySQL query statement below
-# SELECT user_id, name, mail
-# FROM Users
-# WHERE mail REGEXP '@leetcode.com$' 
-    # AND mail REGEXP '^[a-z|A-Z]'
-    # AND mail NOT REGEXP '[#!?~$%^&*(+=)]'
-    # AND mail REGEXP '@?';
 SELECT user_id, name, mail
 FROM Users
 WHERE mail LIKE '%@leetcode.com'
