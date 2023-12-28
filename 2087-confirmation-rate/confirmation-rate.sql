802a0a125046bb68530c715a291407e4d862d214
Lily Wang <awildsheepchase@go.edu.tw>
2023-12-28 21:55:21 +0800

    Time: 2865 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -2,7 +2,7 @@
 SELECT  user_id,
         ROUND(SUM(co)/COUNT(user_id),2) AS confirmation_rate
 FROM(
-SELECT S.user_id AS user_id, C.action AS act,
+SELECT S.user_id AS user_id,
         CASE WHEN action = 'confirmed' THEN 1
         ELSE 0 END AS co
 FROM Signups S LEFT JOIN Confirmations C ON S.user_id = C.user_id) AS Temp
