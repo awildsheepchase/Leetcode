2791546dfe4549cb2f4e7329303a3f847bf24042
Lily Wang <awildsheepchase@go.edu.tw>
2023-12-28 15:53:51 +0800

    Time: 2308 ms (54.42%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -6,14 +6,13 @@ FROM
     FROM MovieRating R LEFT JOIN Users U ON R.user_id = U.user_id
     GROUP BY name
     ORDER BY C DESC,U.name ASC) AS A
-
 UNION ALL
-
 SELECT
     IF(HI = MAX(HI),title,title) AS results
 FROM(
     SELECT AVG(MR.rating) AS HI,title
     FROM MovieRating MR LEFT JOIN Movies M ON MR.movie_id = M.movie_id
-    WHERE MR.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+    WHERE created_at BETWEEN '2020-02-01' AND '2020-02-29'
     GROUP BY MR.movie_id
     ORDER BY HI DESC, M.title ASC) AS B;
+# If you want to keep all rows, including duplicates, use UNION ALL.
