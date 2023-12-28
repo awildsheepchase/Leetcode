77d9a63efea2ae207fcedf6f55a8e82559fd1d6c
Lily Wang <awildsheepchase@go.edu.tw>
2023-12-28 21:54:17 +0800

    Time: 1557 ms (15.59%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,10 +1,9 @@
 # Write your MySQL query statement below
-
-SELECT id AS user_id ,
-        ROUND(SUM(co)/COUNT(id),2) AS confirmation_rate
+SELECT  user_id,
+        ROUND(SUM(co)/COUNT(user_id),2) AS confirmation_rate
 FROM(
-SELECT S.user_id AS id, C.action AS act,
+SELECT S.user_id AS user_id, C.action AS act,
         CASE WHEN action = 'confirmed' THEN 1
         ELSE 0 END AS co
 FROM Signups S LEFT JOIN Confirmations C ON S.user_id = C.user_id) AS Temp
-GROUP BY id;
+GROUP BY user_id;
