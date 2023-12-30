46319abea83830a7af2dc5da808aa124bc91ad99
Lily Wang <awildsheepchase@go.edu.tw>
2023-12-31 00:35:21 +0800

    Time: 875 ms (58.77%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT name
 FROM customer
-WHERE (referee_id NOT LIKE '2' OR referee_id IS NULL) ; 
+WHERE referee_id NOT LIKE '2' OR referee_id IS NULL ; 
